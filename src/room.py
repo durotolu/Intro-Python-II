c1537f248f450e1dd644f75c0442aa1d2bdabc7a
Modurotolu Olokode <durolawk@gmail.com>
2020-02-13 21:29:38 +0100

    added lists array to room class

--- src/room.py
+++ src/room.py
@@ -2,9 +2,10 @@
 # description attributes.
 
 class Room:
-    def __init__(self, name, description):
+    def __init__(self, name, description, listItems=[]):
         self.name = name
         self.description = description
+        self.listItems = listItems
 
     # def __str__(self):
     #     return self.name + ';' + self.description
